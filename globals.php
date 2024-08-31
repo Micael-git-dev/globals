6757c481d9db10eadd0854a7be650c8bb3ba6e2c
Micael-git-dev <micaels3nac@gmail.com>
2024-08-30 21:45:22 -0300

    Add files via upload

---
+++ globals.php
@@ -0,0 +1,14 @@
+<?php
+
+    $v = 100;
+    echo $GLOBALS["v"];
+    echo $v;
+
+    echo $_SERVER['PHP_SELF'];
+    echo $_SERVER['HTTP_HOST'];
+    echo $_SERVER['SERVER_NAME'];
+    echo $_SERVER['HTTP_USER_AGENT'];
+    echo $_REQUEST['firstname'];
+
+
+?>
